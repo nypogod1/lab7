62ba7d5340feb159a380cdfb13403a31519f5c79
DayTon <grig_vlasov18@mail.ru>
2026-01-02 15:01:22 +0300

    Update models.py

--- domain/models.py
+++ domain/models.py
@@ -3,3 +3,35 @@
 class Money:
     amount: Decimal      # сумма (100.50)
     currency: str = "USD" # валюта
+# Например: "iPhone 13, цена $999, количество 1"
+class OrderLine:
+    product_id: str     # ID товара
+    product_name: str   # Название
+    price: Money        # Цена (объект Money)
+    quantity: int       # Количество
+    
+    def total_price(self):
+        return price * quantity  # считаем общую стоимость позиции
+class Order:
+    def __init__(self, id, customer_id, lines, status):
+        self.id = id          # уникальный номер заказа
+        self.customer_id = id # кто заказал
+        self.lines = lines    # список позиций
+        self.status = status  # статус (создан, оплачен, отменен)
+    
+    # БИЗНЕС-ПРАВИЛА (инварианты):
+    
+    # Правило 1: Нельзя оплатить пустой заказ
+    def pay(self):
+        if not self.lines:
+            raise Exception("Заказ пуст!")
+        
+    # Правило 2: Нельзя оплатить дважды
+    def pay(self):
+        if self.status == "PAID":
+            raise Exception("Заказ уже оплачен!")
+        
+    # Правило 3: После оплаты нельзя менять заказ
+    def add_line(self, product):
+        if self.status == "PAID":
+            raise Exception("Нельзя менять оплаченный заказ!")
